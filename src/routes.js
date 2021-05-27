bb73ddc628f32296cdd4a1991d08de9cc8313a08
kahiko <jmkjoshua@gmail.com>
2021-05-27 20:39:31 +0300

    updated

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
 //Page error
-const PageError = React.lazy(() => import('./pages/Pages/Special/PageError'));
+// const PageError = React.lazy(() => import('./pages/Pages/Special/PageError'));
 
 //Shop
 const Shop = React.lazy(() => import('./pages/Shop/index'));
-const ShopProducts = React.lazy(() => import('./pages/Pages/Shop/ShopProducts'));
-const ShopProductDetail = React.lazy(() => import('./pages/Pages/Shop/ShopProductDetail'));
-const ShopCart = React.lazy(() => import('./pages/Pages/Shop/ShopCart'));
-const ShopCheckouts = React.lazy(() => import('./pages/Pages/Shop/ShopCheckouts'));
-const ShopMyAccount = React.lazy(() => import('./pages/Pages/Shop/ShopMyAccount'));
+// const ShopProducts = React.lazy(() => import('./pages/Pages/Shop/ShopProducts'));
+// const ShopProductDetail = React.lazy(() => import('./pages/Pages/Shop/ShopProductDetail'));
+// const ShopCart = React.lazy(() => import('./pages/Pages/Shop/ShopCart'));
+// const ShopCheckouts = React.lazy(() => import('./pages/Pages/Shop/ShopCheckouts'));
+// const ShopMyAccount = React.lazy(() => import('./pages/Pages/Shop/ShopMyAccount'));
 
 const routes = [
 
     //Shop
-    { path: '/shop-products', component: ShopProducts },
-    { path: '/shop/product-detail/:id', component: ShopProductDetail },
-    { path: '/shop-cart', component: ShopCart },
-    { path: '/shop-checkouts', component: ShopCheckouts },
-    { path: '/shop-myaccount', component: ShopMyAccount },
+    // { path: '/shop-products', component: ShopProducts },
+    // { path: '/shop/product-detail/:id', component: ShopProductDetail },
+    // { path: '/shop-cart', component: ShopCart },
+    // { path: '/shop-checkouts', component: ShopCheckouts },
+    // { path: '/shop-myaccount', component: ShopMyAccount },
     
     //Index Main
     { path: '/index', component: Shop },
@@ -26,7 +26,7 @@ const routes = [
     //Index root
     
     { path: '/', component: Shop, exact : true }, 
-    { component: PageError, isWithoutLayout : true, exact : false },
+    // { component: PageError, isWithoutLayout : true, exact : false },
     
 ];
 
